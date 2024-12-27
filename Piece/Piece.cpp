1c7e607bcd1cb90c3b46e479f015d27ae5ed8db9
Cruz Morgan <cruphobia@gmail.com>
2024-12-27 13:27:44 -0800

    more validity for the move getting has been gotten, gotten real good

--- Piece/Piece.cpp
+++ Piece/Piece.cpp
@@ -1,6 +1,9 @@
 #include "Piece.h"
 
+#include "../Board/Board.h"
 #include "../Move/PotentialMove.h"
+
+class Square;
 
 std::vector<ValidMove> Piece::get_valid_moves(Board& board)
 {
@@ -10,10 +13,13 @@ std::vector<ValidMove> Piece::get_valid_moves(Board& board)
         {
             continue;
         }
-        
+
         for (int i = 1; i <= potential_move.repetitions_limit || potential_move.repetitions_limit == 0; i++)
         {
-            
+            int checked_delta_x = potential_move.delta_x * i;
+            int checked_delta_y = potential_move.delta_y * i;
+            auto [success, piece] = board.try_get_piece(checked_delta_x, checked_delta_y);
+            if (success)
         }
     }
 }
