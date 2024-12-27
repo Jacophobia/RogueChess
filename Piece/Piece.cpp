468f01037fa0159a55fd2cbafde6774c20b6387d
Cruz Morgan <cruphobia@gmail.com>
2024-12-27 13:16:13 -0800

    progress on get_valid_moves!!!!111!1!!11!1!!1

--- Piece/Piece.cpp
+++ Piece/Piece.cpp
@@ -1,8 +1,21 @@
 #include "Piece.h"
 
+#include "../Move/PotentialMove.h"
+
 std::vector<ValidMove> Piece::get_valid_moves(Board& board)
 {
-    
+    for (PotentialMove potential_move : potential_moves)
+    {
+        if (potential_move.is_only_valid_on_first_move && has_moved)
+        {
+            continue;
+        }
+        
+        for (int i = 1; i <= potential_move.repetitions_limit || potential_move.repetitions_limit == 0; i++)
+        {
+            
+        }
+    }
 }
 
 Color Piece::get_color()
@@ -16,6 +29,4 @@ void Piece::increment_turn(const bool is_piece_moved)
     {
         has_moved = true;
     }
-
-    
 }
